de147b0c26d7ecb0f07a9bb2430570b436d4c706
Vivek Bhargav <gomchikbhoka@gmail.com>
2016-06-05 08:33:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook' : {
-        clientID: '569706209873246',
-        clientSecret: 'cc8b4ecae2e7a91bcad685e4979c6c2a',
+        clientID: 'enter fb client id here',
+        clientSecret: 'enter fb client secret here',
         callbackURL: 'https://localhost:3443/users/facebook/callback'
     }
 }
